cdb500ac2d3d6f7c7cbdd8e43421a49da0500309
hajo <hajo@chalmers.se>
2014-12-18 11:34:03 +0100

    fix

--- src/main/java/edu/chl/ooproject/view/ProjectView.java
+++ src/main/java/edu/chl/ooproject/view/ProjectView.java
@@ -9,12 +9,10 @@ import javax.swing.SwingConstants;
 
 public class ProjectView extends JFrame {
 
-    private JButton ylfva;
-    private JButton ylfva2;
+   
     private final JButton button = new JButton(Project.PROJECT_BUTTON_TEXT);
     private final JLabel pressesLabel;
-    private JLabel byHajo;
-    private JLabel byHajo2;
+    
 
     public ProjectView(Project project) {
         super(Project.PROJECT_WINDOW_TEXT);
