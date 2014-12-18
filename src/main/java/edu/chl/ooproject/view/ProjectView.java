654ebe4e88b171053c771e895372cc1bf96b4b70
hajo <hajo@chalmers.se>
2014-12-18 11:46:28 +0100

    fix

--- src/main/java/edu/chl/ooproject/view/ProjectView.java
+++ src/main/java/edu/chl/ooproject/view/ProjectView.java
@@ -12,7 +12,11 @@ public class ProjectView extends JFrame {
    
     private final JButton button = new JButton(Project.PROJECT_BUTTON_TEXT);
     private final JLabel pressesLabel;
+
     private JLabel lbl22;
+
+    private JLabel lbl2;
+
 
     public ProjectView(Project project) {
         super(Project.PROJECT_WINDOW_TEXT);
